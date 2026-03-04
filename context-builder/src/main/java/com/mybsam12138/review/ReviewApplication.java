6057ba9631da541dfef17fd52368797e8288cc7f
Sam <mybsam12@gmail.com>
2026-03-04 10:56:27 +0800

    test the review

--- context-builder/src/main/java/com/mybsam12138/review/ReviewApplication.java
+++ context-builder/src/main/java/com/mybsam12138/review/ReviewApplication.java
@@ -1,9 +1,11 @@
 package com.mybsam12138.review;
 
+import org.springframework.ai.model.openai.autoconfigure.OpenAIAutoConfigurationUtil;
+import org.springframework.ai.model.openai.autoconfigure.OpenAiImageAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {OpenAiImageAutoConfiguration.class})
 public class ReviewApplication {
 
     public static void main(String[] args) {
